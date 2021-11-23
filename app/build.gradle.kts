716fb9bf36072a0d5a135aae091555c948bbb72d
GeanCarlosBrandao <82841776+GeanCarlosBrandao@users.noreply.github.com>
2021-11-22 21:06:57 -0300

    Create First App Theme using material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,10 +53,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:${Androidx.appcompat}")
     implementation("com.google.android.material:material:${Google.material}")
     implementation("androidx.compose.ui:ui:${Androidx.Compose.ui}")
-    implementation("androidx.compose.material:material:${Androidx.Compose.material}")
+//    implementation("androidx.compose.material:material:${Androidx.Compose.material}")
     implementation("androidx.compose.ui:ui-tooling-preview:${Androidx.Compose.toolingPreview}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${Androidx.lifecycleRuntime}")
     implementation("androidx.activity:activity-compose:${Androidx.activityCompose}")
+    implementation("androidx.compose.material3:material3:${Androidx.Compose.material3}")
+
+
     testImplementation ("junit:junit:$junitVersion")
     androidTestImplementation("androidx.test.ext:junit:${Androidx.Test.junit}")
     androidTestImplementation("androidx.test.espresso:espresso-core:${Androidx.Test.espresso}")
