4fb77838feab19d97a97a4d3e2c17369a2674555
GeanCarlosBrandao <82841776+GeanCarlosBrandao@users.noreply.github.com>
2021-11-25 18:17:28 -0300

    Add layout expenses

--- buildSrc/src/main/kotlin/AppDependenciesVersions.kt
+++ buildSrc/src/main/kotlin/AppDependenciesVersions.kt
@@ -1,10 +1,10 @@
-const val composeVersion = "1.0.1"
-const val kotlinVersion = "1.5.21"
+const val composeVersion = "1.0.5"
+const val kotlinVersion = "1.5.31"
 const val junitVersion = "4.13.2"
 
 object Androidx {
     const val core = "1.7.0"
-    const val appcompat = "1.3.1"
+    const val appcompat = "1.4.0"
     const val lifecycleRuntime = "2.4.0"
     const val activityCompose = "1.4.0"
     const val navigation = "2.4.0-beta02"
@@ -16,6 +16,7 @@ object Androidx {
 
     object Compose {
         const val ui = composeVersion
+        const val uiTest = "1.1.0-beta03"
         const val material = composeVersion
         const val material3 = "1.0.0-alpha01"
         const val toolingPreview = composeVersion
